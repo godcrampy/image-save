937f6d085387bb998d3a610c23e3162e5e44e934
Sahil <sahilbondre@gmail.com>
2019-09-07 19:45:56 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+const multer = require("multer");
+const fs = require("fs");
+const imagemin = require("imagemin");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+
+const port = process.env.PORT || 3000;
+
+
+// Express Config
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/public"));
+
+const upload = multer({
+  dest: path.resolve(__dirname, "temporary")
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const temporaryPath = req.file.path;
+  const targetPath = path.join(__dirname, `./temporary/${req.file.filename}.${path.extname(req.file.originalname).toLowerCase()}`);
+
+  fs.rename(temporaryPath, targetPath, err => {
+    if (err) return handleError(err, res);
+    res
+      .status(200)
+      .contentType("text/plain")
+      .end("File uploaded!");
+  });
+
+  // compress images
+  (async () => {
+    await imagemin(["temporary/*.{jpg,png}"], {
+      destination: "build/",
+      plugins: [
+        imageminMozjpeg({
+          quality: 30
+        }),
+        imageminPngquant({
+          quality: [0.3, 0.4]
+        })
+      ]
+    });
+
+    // Empty original directory
+    const directory = path.join(__dirname, "/temporary");
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+  })();
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
